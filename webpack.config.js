846365f4e4ef9b944f163d25414fe1a48965887f
Giacomo Secchi <giacomosecchi@gmail.com>
2025-08-08 18:14:32 +0200

    chore: update dependencies and adjust webpack entry points
    - Bump versions of various WordPress packages and 10up/block-components in package.json for improved functionality and compatibility.
    - Update webpack.config.js to use direct paths for entry points instead of the helper function from @wordpress/scripts.

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,11 @@
 // Import the original config from the @wordpress/scripts package.
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
-// Import the helper to find and generate the entry points in the src directory
-const { getWordPressSrcDirectory } = require( '@wordpress/scripts/utils' );
-
 module.exports = {
 	...defaultConfig,
 	entry: {
 		...defaultConfig.entry(),
-		'plugin-jetpack': `./${ getWordPressSrcDirectory() }/plugins/jetpack`,
-		popup: `./${ getWordPressSrcDirectory() }/extensions/buttons/index.js`,
+		'plugin-jetpack': `./src/plugins/jetpack`,
+		popup: `./src/extensions/buttons/index.js`,
 	},
 };
