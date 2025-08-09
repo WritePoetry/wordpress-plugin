416034c8a69eb786b30d39b10ea02489c1193351
Giacomo Secchi <giacomosecchi@gmail.com>
2025-08-09 11:30:36 +0200

    chore: update plugin metadata and improve documentation

--- write-poetry.php
+++ write-poetry.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Write Poetry
  * Plugin URI:        https://github.com/WritePoetry/wordpress-plugin
  * Description:       The Swiss knife plugin designed for developers and advanced users. Unlock the full potential of WordPress with this versatile tool. Empower your workflow without getting your hands dirty.
- * Version:           0.3.16
+ * Version:           0.3.17
  * Requires at least: 5.9
  * Requires PHP:      7.2
  * Author:            Giacomo Secchi
@@ -24,9 +24,6 @@
  * Release Asset:     true
  */
 
-add_filter( 'gul_update_server', function () {
-    return 'https://overt-advice.localsite.io/';
-});
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
@@ -38,36 +35,38 @@ if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
 
-
+add_filter( 'gul_update_server', function () {
+    return 'https://overt-advice.localsite.io';
+} );
 ( new \Fragen\Git_Updater\Lite( __FILE__ ) )->run();
 
-// /**
-//  * The code that runs during plugin activation
-//  */
-// register_activation_hook(
-// 	__FILE__,
-// 	function () {
-// 		WritePoetry\Base\Activate::activate();
-// 	}
-// );
+/**
+ * The code that runs during plugin activation
+ */
+register_activation_hook(
+	__FILE__,
+	function () {
+		WritePoetry\Base\Activate::activate();
+	}
+);
 
 
-// /**
-//  * The code that runs during plugin deactivation
-//  */
-// register_deactivation_hook(
-// 	__FILE__,
-// 	function () {
-// 		WritePoetry\Base\Deactivate::deactivate();
-// 	}
-// );
+/**
+ * The code that runs during plugin deactivation
+ */
+register_deactivation_hook(
+	__FILE__,
+	function () {
+		WritePoetry\Base\Deactivate::deactivate();
+	}
+);
 
 
-// /**
-//  * Initialize all the core classes of the plugin
-//  *
-//  * @since 0.2.2
-//  */
-// if ( class_exists( 'WritePoetry\\Init' ) ) {
-// 	WritePoetry\Init::register_services();
-// }
+/**
+ * Initialize all the core classes of the plugin
+ *
+ * @since 0.2.2
+ */
+if ( class_exists( 'WritePoetry\\Init' ) ) {
+	WritePoetry\Init::register_services();
+}
