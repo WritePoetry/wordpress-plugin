38252aeaa271eb2a69ae0741fa31ba672c5098a9
Giacomo Secchi <giacomosecchi@gmail.com>
2025-08-09 19:18:43 +0200

    chore: update plugin version to 0.3.22

--- writepoetry-plugin.php
+++ writepoetry-plugin.php
@@ -11,7 +11,7 @@
  * Plugin Name:       WritePoetry
  * Plugin URI:        https://github.com/WritePoetry/writepoetry-plugin
  * Description:       The Swiss knife plugin designed for developers and advanced users. Unlock the full potential of WordPress with this versatile tool. Empower your workflow without getting your hands dirty.
- * Version:           0.3.21
+ * Version:           0.3.22
  * Requires at least: 5.9
  * Requires PHP:      7.2
  * Author:            Giacomo Secchi
@@ -19,7 +19,6 @@
  * Text Domain:       write-poetry
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Update URI:        https://github.com/WritePoetry/writepoetry-plugin/
  * GitHub Plugin URI: https://github.com/WritePoetry/writepoetry-plugin
  * Primary Branch:    trunk
  * Release Asset:     true
